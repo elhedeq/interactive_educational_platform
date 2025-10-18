075e6ae9b22fec8e2f2f3fbfedc69102ffc533e1
Mona Lisa <88691666+bashmoo@users.noreply.github.com>
2025-10-18 12:57:31 +0300

    Removed Tailwind, added Bootstrap setup

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component,HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+
+  // menue of user profile if we going to open or close it
+  isProfileMenueOpen=false
+  toggleMenu(){
+     this.isProfileMenueOpen=!this.isProfileMenueOpen
+  }
+
+  @HostListener('document:click',['$event'])
+  clickOutside(event:MouseEvent){
+    const target=event.target as HTMLElement
+    if (!target.closest('.ml-3')) {
+      this.isProfileMenueOpen = false;
+    }
+  }
+
+  closeMenue(){
+   this.isProfileMenueOpen=false
+  }
+
+
 
 }
