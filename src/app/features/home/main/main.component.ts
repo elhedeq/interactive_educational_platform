591cb0236c7f5f12883aad0171c33479fe118b5c
Mona Lisa <88691666+bashmoo@users.noreply.github.com>
2025-10-23 19:35:19 +0300

    modifying home component

--- src/app/features/home/main/main.component.ts
+++ src/app/features/home/main/main.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-main',
   standalone: true,
   imports: [],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.css'
+  styleUrls: ['./main.component.css']
 })
 export class MainComponent {
 
